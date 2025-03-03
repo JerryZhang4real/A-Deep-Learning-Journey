9c7831cd56fa4490ff842cf2c648e6f1a964b847
JiaruiZhang <zhangjiarui_0209@163.com>
2025-03-03 02:08:12 -0500

    test MLP created from scratch

---
+++ __main__.py
@@ -0,0 +1,72 @@
+import modules
+import numpy as np
+
+
+n = modules.MLP(3, [4, 4, 1])
+
+xs = [
+  [2.0, 3.0, -1.0],
+  [3.0, -1.0, 0.5],
+  [0.5, 1.0, 1.0],
+  [1.0, 1.0, -1.0],
+]
+ys = [1.0, -1.0, -1.0, 1.0] # desired targets
+
+for k in range(300):
+  
+  # forward pass
+  ypred = [n(x) for x in xs]
+  loss = sum((yout - ygt)**2 for ygt, yout in zip(ys, ypred))
+
+  # backward pass
+  n.zero_grad()
+  loss.backward()
+  
+  # update
+  for p in n.parameters():
+    p.data += -0.01 * p.grad
+  
+  if k % 10 == 0 : print(int(k/10), loss.data)
+
+print([n(x) for x in xs])
+
+
+# draw the computation graph
+from graphviz import Digraph
+
+def trace(root):
+  # builds a set of all nodes and edges in a graph
+  nodes, edges = set(), set()
+  def build(v):
+    if v not in nodes:
+      nodes.add(v)
+      for child in v._prev:
+        edges.add((child, v))
+        build(child)
+  build(root)
+  return nodes, edges
+
+def draw_dot(root):
+  dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'}) # LR = left to right
+  
+  nodes, edges = trace(root)
+  for n in nodes:
+    uid = str(id(n))
+    # for any value in the graph, create a rectangular ('record') node for it
+    dot.node(name = uid, label = "{ %s | data %.4f | grad %.4f }" % (n.label, n.data, n.grad), shape='record')
+    if n._op:
+      # if this value is a result of some operation, create an op node for it
+      dot.node(name = uid + n._op, label = n._op)
+      # and connect this node to it
+      dot.edge(uid + n._op, uid)
+
+  for n1, n2 in edges:
+    # connect n1 to the op node of n2
+    dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+
+  return dot
+
+x = [2.0, 3.0, -1.0]
+y = n(x)
+y.backward()
+draw_dot(y).render('computation_graph', format='png', cleanup=True, view=True)
